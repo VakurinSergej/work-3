7a1e1aac622831ce6910f3252cc5dce1eaa517f3
VakurinSergej <120665026+VakurinSergej@users.noreply.github.com>
2023-01-08 22:45:29 +0300

    works in lesson 3
    My works in lesson 3

---
+++ work-3-2.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите первое число: ");
+double xa = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double ya = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+double za = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите четвертое число: ");
+
+double xb = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите пятое число: ");
+double yb = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите шестое число: ");
+double zb = double.Parse(Console.ReadLine());
+
+double ab = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+{
+    Console.WriteLine("\"Вот расстояние между этими точками в квадрониуме:\"");
+}
+Console.WriteLine(ab);
---
+++ work-3-3.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= N)
+{
+    Console.WriteLine(" " + i * i * i);
+    i++;
+}
